1096ca59bb04088fff62e8692520f99aa95623e8
박상민 <pp8817@naver.com>
2025-03-23 15:47:23 +0900

    Time: 3 ms (97.12%), Space: 18.8 MB (35.18%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,10 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        N = len(nums)
-        dp = [int(1e9)]*N # 각 인덱스까지 점프 횟수
-        dp[0]=0
-        for i in range(N):
-            for j in range(1, nums[i]+1):
-                if i+j<N:
-                    dp[i+j] = min(dp[i+j], dp[i]+1)
-        return dp[N-1]
+        temp = now = count = 0
+        
+        while now < len(nums)-1:
+            max_ = 0
+            for i in range(temp, now+1):
+                max_ = max(max_, i+nums[i])
+            temp = now+1
+            now = max_
+            count += 1
+        return count
