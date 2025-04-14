b3e9ef17bb3d3c07b7604f1d5d7b0d62acc39b1d
박상민 <pp8817@naver.com>
2025-04-15 02:53:01 +0900

    Time: 0 ms (100%), Space: 17.7 MB (83.82%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,24 @@
+class Solution:
+    def summaryRanges(self, nums):
+        result = []
+        if not nums:
+            return result
+
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1] + 1:
+                end = nums[i - 1]
+                if start == end:
+                    result.append(str(start))
+                else:
+                    result.append(f"{start}->{end}")
+                start = nums[i]
+
+        end = nums[-1]
+        if start == end:
+            result.append(str(start))
+        else:
+            result.append(f"{start}->{end}")
+
+        return result
