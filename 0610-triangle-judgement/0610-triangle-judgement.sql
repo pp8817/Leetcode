0249b7b75cafc1924f5b27057a993e25fe1e2848
박상민 <pp8817@naver.com>
2026-03-05 23:50:01 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,11 +1,7 @@
-SELECT x, y, z,
+SELECT
+    x, y, z,
     CASE
-        WHEN cnt = 1 THEN 'Yes' ELSE 'No'
+        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+        ELSE 'No'
     END AS triangle
-FROM (
-    SELECT 
-        CASE
-            WHEN x + y > z AND x + z > y AND z + y > x THEN 1 ELSE 0
-        END AS cnt, x, y, z
-    FROM Triangle
-) t
+FROM Triangle;
