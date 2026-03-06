727bbe7fc7dd699f1f9c4e254fb5f4393a828729
박상민 <pp8817@naver.com>
2026-03-06 15:24:31 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,15 +1,11 @@
-SELECT id, SUM(cnt1) as num
+SELECT id, COUNT(*) AS num
 FROM (
-    (SELECT requester_id as id, COUNT(*) AS cnt1
+    SELECT requester_id AS id
     FROM RequestAccepted
-    GROUP BY requester_id)
-
     UNION ALL
-
-    (SELECT accepter_id as id, COUNT(*) AS cnt1
+    SELECT accepter_id AS id
     FROM RequestAccepted
-    GROUP BY accepter_id)
-    ) t
+) t
 GROUP BY id
 ORDER BY num DESC
 LIMIT 1;
